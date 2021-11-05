9c1a49072050691a602e27c2d8a2f08f1a5a77bd
Damian Nadales <damian.only@gmail.com>
2021-11-05 13:32:03 +0100

    Add my org mode config
    This config was migrated from https://github.com/dnadales/emacs.d/blob/f25db51d2dc86a89009437ff7c133bffcf36f9ce/org_config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -29,8 +29,58 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
+(setq org-directory "~/psys/")
+(setq inbox-path (concat org-directory "inbox.org"))
+(setq calendar-path (concat org-directory "calendar.org"))
+(setq projects-path (concat org-directory "projects.org"))
+(setq next-actions-path (concat org-directory "next-actions.org"))
+(after! org
+  (setq  org-agenda-files (list calendar-path)
+         ;; This adds a ``CLOSED'' label to the TODO entry, which describes the
+         ;; date and time in which the activity was marked as done.
+         org-log-done t
+         ;; My capture templates
+         org-capture-templates
+        '(("t" "New Task" entry (file inbox-path) "* TODO %?\n  %U\n  %i\n"))
+         ;; See https://www.gnu.org/software/emacs/manual/html_node/org/Tracking-TODO-state-changes.html
+         ;;
+         ;; Meaning of symbols
+         ;; @: leave a note
+         ;; !: add a timestamp when entering that state
+         ;;
+         org-todo-keywords
+         '((sequence "TODO(t!)" "WAITING(w@/!)" "STARTED(s!/!)" "DELEGATED(e@/!)" "|" "DONE(d!)" "CANCELED(c@/!)"))
+         ;; Agenda view customizations
+         org-agenda-prefix-format
+         '((agenda . "%?-12t%-11s")
+         (todo . " %i %-12:c")
+         (tags . " %i %-12:c")
+         (search . " %i %-12:c"))
+         org-agenda-todo-keyword-format "·"
+         org-agenda-skip-scheduled-if-done t
+         org-enforce-todo-dependencies t
+         org-hide-leading-stars t
+         org-log-into-drawer t
+         org-odd-levels-only t
+         ;; Refile configuration
+         org-refile-use-outline-path 'file ; Use the file path as of the
+                                           ; refiling targets. This makes it
+                                           ; possible to refile nodes to the
+                                           ; top-level.
+         org-outline-path-complete-in-steps nil
+         org-refile-targets (quote ((projects-path :level . 1)
+                                 (next-actions-path :level . 1)
+                                 (calendar-path :level . 1)))
+         ;; Custom agenda commands
+         org-agenda-custom-commands `(;; match those tagged with :inbox:, are not scheduled, are not DONE.
+                                       ("u" "[u]nscheduled tasks" tags "-SCHEDULED={.+}/!+TODO|+STARTED|+WAITING"))
+         )
+  ;; Org-babel supported languages
+  (org-babel-do-load-languages
+   'org-babel-load-languages '((haskell . t)
+                               (dot . t))
+   )
+  )
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
